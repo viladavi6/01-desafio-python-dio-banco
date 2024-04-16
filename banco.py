57fe947fc7ecd5af4ae3475f60bb3e008b0044e3
Davi Vila <viladavi6@gmail.com>
2024-04-16 10:53:52 -0300

    Melhorias adicionando funções, atividade 2 do curso

--- banco.py
+++ banco.py
@@ -16,29 +16,22 @@ def menu():
 ================================
 ''')
 
-
 def retornar_menu():
-        print('\nRetornando ao menu...')
-        time.sleep(5)
-        
-    
-def deposito(total_saldo, valor, extrato, /):
-    
-   if valor > 0:
+    print('\nRetornando ao menu...')
+    time.sleep(5)
+
+def deposito(total_saldo, valor, extrato):
+    if valor > 0:
         total_saldo += valor
         extrato += f'\nDepósito realizado com sucesso: {valor:.2f}\n'
         print(f'\n{extrato}\n')
-        return total_saldo, extrato
-    
+    return total_saldo, extrato
 
 def sacar(*, saque, total_saldo, extrato, limite_saque_valor, limite_saque_dia):
-    
     sem_saldo = total_saldo < saque
     sem_limite = saque > limite_saque_valor
     sem_saque = limite_saque_dia == 0
 
-    
-    
     if sem_limite:
         print(f'''
                 ===================================================
@@ -50,7 +43,6 @@ def sacar(*, saque, total_saldo, extrato, limite_saque_valor, limite_saque_dia):
                   
                 ===================================================
                 ''')
-
     elif sem_saldo:
         print(f'''
                 ===================================================
@@ -62,7 +54,6 @@ def sacar(*, saque, total_saldo, extrato, limite_saque_valor, limite_saque_dia):
                 ===================================================
                   
                 ''')
-        
     elif sem_saque:
         print('''
                     ===================================================
@@ -73,7 +64,6 @@ def sacar(*, saque, total_saldo, extrato, limite_saque_valor, limite_saque_dia):
                     ===================================================
                   
                   ''')
-        
     elif saque > 0:
         total_saldo -= saque
         limite_saque_dia -= 1
@@ -89,220 +79,170 @@ def sacar(*, saque, total_saldo, extrato, limite_saque_valor, limite_saque_dia):
                 \n  
                 '''
         print(f'\n{extrato}\n')
-    
     return total_saldo, extrato, limite_saque_dia
-            
 
-def extratos(total_saldo, /, *, extrato):
-    
+def extratos(total_saldo, *, extrato):
     if extrato == "":
         print(f'\nVocê tem R$ 0 disponível.')
     else:
         print(f'\nVocê tem R${total_saldo:,.2f} disponível.')
         print(f'\nAqui estão suas últimas movimentações: \n{extrato}\n')
 
+def filtrar_usuario(cpf, usuario):
+    usuarios_filtrados = [usuarios for usuarios in usuario if usuarios['CPF'] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
 
-def criacao_usuario(usuario):
+def criar_conta(usuario, AGENCIA, numero_conta_final):
+    print('\nPara cadastrar uma conta é necessário ter criado um usuário: ')
+    print('\nCaso queira voltar ao menu é só digitar 0 em qualquer operação: ')
+    time.sleep(5)
     
     while True:
         try:
-            print('\nCaso queira voltar ao menu é só digitar 0 em qualquer operação: ')
-            time.sleep(5)
-            
-            nome = str(input('\nInforme seu nome completo: ')) 
-            if nome in '0': 
-                retornar_menu()
-                break
-            
-            data_nascimento = str(input('\nInforme sua data de nascimento neste formato (d/m/a): '))
-            if data_nascimento in '0':
-                retornar_menu()
-                break
-        
-            cpf = str(input('\nInforme somente os números do seu CPF(123.456.789-12): '))
-            if cpf in '0':
-                retornar_menu()
-                break
-            
-            rua = str(input('\nInforme seu endereço (Rua): '))
-            if rua in '0':
-                retornar_menu()
-                break
-            
-            numero = str(input('\nInforme o número da sua residência: '))
-            if numero in '0':
-                retornar_menu()
-                break
-            
-            bairro = str(input('\nInforme o seu bairro: '))
-            if bairro in '0':
-                retornar_menu()
-                break
-            
-            cidade = str(input('\nInforme a sua cidade: '))
-            if cidade in '0':
-                retornar_menu()
-                break
-            
-            uf = str(input('\nInforme seu estado(UF): '))
-            if uf in '0':
-                retornar_menu()
-                break
-            
-            else:
-                print(f'\nUsuário Cadastrado com Sucesso. Obrigado pela preferência!\n')
-                usuario.append({'Nome': nome, 'Data de Nascimento': data_nascimento, 'CPF': cpf, 'Rua': rua, 'Número': numero, 'Bairro': bairro, 'Cidade': cidade, 'UF': uf})
-                retornar_menu()
-                break
-                
-        except ValueError:
-            print('\nInsira um valor válido: ')
-                
-    
-def filtrar_usuario(*, nome, data_nascimento, endereco, cpf, agencia, usuario, numero_conta_final):
-    
-    usuario = ({'Nome': nome, 'Data de Nascimento': data_nascimento, 'CPF': cpf, 'Endereço': endereco, 'Agência': agencia, 'Número da conta': numero_conta_final})
-
-
-def criar_conta(agencia, numero_conta_final, usuario):
-    
-    while True:
-        try:
-            print('\nPara cadastrar uma conta é necessário ter criado um usuário: ')
-            print('\nCaso queira voltar ao menu é só digitar 0 em qualquer operação: ')
-            time.sleep(5)
             cpf = input('\nDigite o seu CPF para prosseguir: ')
+            usuario = filtrar_usuario(cpf, usuario)
+            
             if cpf == '0':
                 retornar_menu()
-                break
-        except ValueError:
-            print('\nDigite um cpf válido, ou cadastre seu usuário.')
-    
-    if usuario == filtrar_usuario(cpf, usuario):
-        print('\nCriando dados bancários: ')
-                
-        while True:
-            agencia = rd.randint(1000, 9999)
-            numero_conta = rd.randint(10000, 99999)
-            numero_conta2 = rd.randint(0, 9)
-            numero_conta_final = str(numero_conta) + '-' + str(numero_conta2)
-                    
-            if numero_conta_final != usuario({'Número da conta': numero_conta_final}) or agencia != usuario({'Agencia': agencia}):
+                return None
+            elif not usuario: 
+                print("\nCPF não cadastrado. Por favor, cadastre seu usuário primeiro.")
+                retornar_menu()
+                return None
+            elif usuario:
+                print('\nCriando dados bancários: ')
+                numero_conta = rd.randint(10000, 99999)
+                numero_conta2 = rd.randint(0, 9)
+                numero_conta_final = f"{numero_conta}-{numero_conta2}"
                 print(f'''===============================
           
                                 Conta criada com sucesso:
                                 Obrigado pela preferência!
-                                Agência: {agencia}
+                                Agência: {AGENCIA}
                                 Nº da Conta: {numero_conta_final}
                         
                             ===============================''')
                 retornar_menu()
-                return({'Agência': agencia, 'Número da conta': numero_conta_final}) 
-                break
-                
-            else:
-                agencia = rd.randint(1000, 9999)
-                numero_conta = rd.randint(10000, 99999)
-                numero_conta2 = rd.randint(0, 9)
-                numero_conta_final = str(numero_conta) + '-' + str(numero_conta2)
-                print(f'''===============================
-          
-                                Conta criada com sucesso:
-                                Obrigado pela preferência!
-                                Agência: {agencia}
-                                Nº da Conta: {numero_conta_final}
-                        
-                          ===============================''')
+                return {'Agência': AGENCIA, 'Número da conta': numero_conta_final, 'usuario': usuario} 
+        except ValueError:
+            print('\nDigite um cpf válido, ou cadastre seu usuário.')
+            
+def criacao_usuario(usuario):
+    while True:
+        try:
+            print('\nCaso queira voltar ao menu é só digitar 0 em qualquer operação: ')
+            time.sleep(5)
+            
+            cpf = input('\nInforme somente os números do seu CPF(12345678912): ')
+            usuarios = filtrar_usuario(cpf, usuario)
+            if cpf == '0':
                 retornar_menu()
-                return({'Agência': agencia, 'Número da conta': numero_conta_final}) 
-                break                              
-        
+                break
+            elif usuarios:
+                print('Já existe este usuário')
+                retornar_menu()
+                return None
+            else:
+                nome = input('\nInforme seu nome completo: ')
+                if nome == '0': 
+                    retornar_menu()
+                    break
+                data_nascimento = input('\nInforme sua data de nascimento neste formato (d/m/a): ')
+                if data_nascimento == '0':
+                    retornar_menu()
+                    break
+                rua = input('\nInforme seu endereço (Rua): ')
+                if rua == '0':
+                    retornar_menu()
+                    break
+                numero = input('\nInforme o número da sua residência: ')
+                if numero == '0':
+                    retornar_menu()
+                    break
+                bairro = input('\nInforme o seu bairro: ')
+                if bairro == '0':
+                    retornar_menu()
+                    break
+                cidade = input('\nInforme a sua cidade: ')
+                if cidade == '0':
+                    retornar_menu()
+                    break
+                uf = input('\nInforme seu estado(UF): ')
+                if uf == '0':
+                    retornar_menu()
+                    break
+            
+                print(f'\nUsuário Cadastrado com Sucesso. Obrigado pela preferência!\n')
+                usuario.append({'Nome': nome, 'Data de Nascimento': data_nascimento, 'CPF': cpf, 'Rua': rua, 'Número': numero, 'Bairro': bairro, 'Cidade': cidade, 'UF': uf})
+                retornar_menu()
+                break
+        except ValueError:
+            print('\nInsira um valor válido: ')
 
 def listar_contas(contas):
-    
     for conta in contas:
         linha = f'''
-        Agência:\t{conta['agencia']}
-        C/C:\t\t{conta['numero_conta']}
-        Titular:\t{conta['usuario']['nome']}
+        Agência:\t{conta['Agência']}
+        C/C:\t\t{conta['Número da conta']}
+        Titular:\t{conta['usuario']['Nome']}
         '''
         print('=' * 100)
         print(linha)
-    
 
 def iniciar():
-    
     limite_saque_valor  = 500
     limite_saque_dia = 3
     total_saldo = 0
     extrato = ''
     usuario = [] 
-    contas = []  
+    contas = []
+    AGENCIA = '0001'  
 
-    
     print('\nBem vindo! Qual o serviço que você deseja?\n ')
     
     while True:
-        
         menu()
-            
         opcao = input('\n:')
-            
         if opcao == '1':
-                 
-                while True:
-                    try:
-                        valor = float(input('Digite o valor que você deseja depositar: '))
-                        total_saldo, extrato = deposito(total_saldo, valor, extrato)
-                        retornar_menu()
-                        break
-                    
-                    except ValueError:
-                        print('\nValor incorreto.\n')
-                        
-            
+            while True:
+                try:
+                    valor = float(input('Digite o valor que você deseja depositar: '))
+                    total_saldo, extrato = deposito(total_saldo, valor, extrato)
+                    retornar_menu()
+                    break
+                except ValueError:
+                    print('\nValor incorreto.\n')
         elif opcao == '2':
-                 
             while True:
                 try:   
                     saque = float(input('Digite o valor que você deseja sacar: '))
                     break
                 except ValueError:
                     print('\nDigite um valor válido que deseja sacar.\n')
-                    
             total_saldo, extrato, limite_saque_dia = sacar(
-            total_saldo=total_saldo, 
-            saque=saque,
-            limite_saque_valor=limite_saque_valor,
-            limite_saque_dia=limite_saque_dia,
-            extrato=extrato,)
-            retornar_menu()
-                   
-            
+                total_saldo=total_saldo, 
+                saque=saque,
+                limite_saque_valor=limite_saque_valor,
+                limite_saque_dia=limite_saque_dia,
+                extrato=extrato)
+            retornar_menu()     
         elif opcao == '3':
-            extratos(total_saldo,extrato=extrato,)
+            extratos(total_saldo, extrato=extrato)
             retornar_menu()
-            
-            
         elif opcao == '4':
             criacao_usuario(usuario=usuario)
-            
         elif opcao == '5':
-            criar_conta(usuario=usuario)
-            
+            numero_conta_final = ''
+            conta = criar_conta(usuario, AGENCIA, numero_conta_final)
+            if conta:
+                contas.append(conta)
         elif opcao == '6':
-            listar_contas()
-            
+            listar_contas(contas)
         elif opcao == '0':
             print('\n Obrigado pela preferência, tenha um ótimo dia!')
             break    
-        
         elif opcao not in ('0', '1', '2', '3', '4', '5', '6'):
             print('\nOpção inválida\n')
             retornar_menu()
-                
-            
-
-
 
 iniciar()
